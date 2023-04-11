845997b504f2d88e888216994ced10fe4b7c54e7
betrider <tlsfpem@gmail.com>
2023-04-11 14:47:01 +0900

    isolate 예제 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,85 @@
-// ignore_for_file: unused_local_variable, avoid_print
+// Copyright 2019-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import 'dart:async';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
-import 'package:flutter_isolate/flutter_isolate.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:window_size/window_size.dart';
 
-@pragma('vm:entry-point')
-void isolate2(String arg) {
-  getTemporaryDirectory().then((dir) async {
-    print("isolate2 temporary directory: $dir");
+import 'data_transfer_page.dart';
+import 'infinite_process_page.dart';
+import 'performance_page.dart';
 
-    await FlutterDownloader.initialize(debug: true);
-    FlutterDownloader.registerCallback(AppWidget.downloaderCallback);
-
-    final taskId = await FlutterDownloader.enqueue(
-        url: "https://raw.githubusercontent.com/rmawatson/flutter_isolate/master/README.md", savedDir: dir.path);
-  });
-  Timer.periodic(const Duration(seconds: 1), (timer) => print("Timer Running From Isolate 2"));
+void main() {
+  setupWindow();
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
 }
 
-@pragma('vm:entry-point')
-void isolate1(String arg) async {
-  await FlutterIsolate.spawn(isolate2, "hello2");
+const double windowWidth = 1024;
+const double windowHeight = 800;
 
-  getTemporaryDirectory().then((dir) {
-    print("isolate1 temporary directory: $dir");
-  });
-  Timer.periodic(const Duration(seconds: 1), (timer) => print("Timer Running From Isolate 1"));
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Isolate Example');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+  }
 }
 
-@pragma('vm:entry-point')
-void computeFunction(String arg) async {
-  getTemporaryDirectory().then((dir) {
-    print("Temporary directory in compute function : $dir with arg $arg");
-  });
-}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-void main() async {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Plugin example app'),
+      theme: ThemeData.light(useMaterial3: true),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.flash_on),
+                  text: 'Performance',
+                ),
+                Tab(
+                  icon: Icon(Icons.sync),
+                  text: 'Infinite Process',
+                ),
+                Tab(
+                  icon: Icon(Icons.storage),
+                  text: 'Data Transfer',
+                ),
+              ],
             ),
-            body: const AppWidget()));
-  }
-}
-
-class AppWidget extends StatelessWidget {
-  const AppWidget({super.key});
-
-  static void downloaderCallback(String id, DownloadTaskStatus status, int progress) {
-    print("progress: $progress");
-  }
-
-  Future<void> _run() async {
-    print("Temp directory in main isolate : ${(await getTemporaryDirectory()).path}");
-    final isolate = await FlutterIsolate.spawn(isolate1, "hello");
-    Timer(const Duration(seconds: 5), () {
-      print("Pausing Isolate 1");
-      isolate.pause();
-    });
-    Timer(const Duration(seconds: 10), () {
-      print("Resuming Isolate 1");
-      isolate.resume();
-    });
-    Timer(const Duration(seconds: 20), () {
-      print("Killing Isolate 1");
-      isolate.kill();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child:
-          Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center, children: [
-        ElevatedButton(
-          onPressed: _run,
-          child: const Text('Spawn isolates'),
+            title: const Text('Isolate Example'),
+          ),
+          body: const TabBarView(
+            children: [
+              PerformancePage(),
+              InfiniteProcessPageStarter(),
+              DataTransferPageStarter(),
+            ],
+          ),
         ),
-        ElevatedButton(
-          child: const Text('Check running isolates'),
-          onPressed: () async {
-            final isolates = await FlutterIsolate.runningIsolates;
-            // ignore: use_build_context_synchronously
-            await showDialog(
-              builder: (ctx) {
-                return Center(
-                  child: Container(
-                    color: Colors.white,
-                    padding: const EdgeInsets.all(5),
-                    child: Column(
-                      children: isolates.map((i) => Text(i)).cast<Widget>().toList() +
-                          [
-                            ElevatedButton(
-                                child: const Text("Close"),
-                                onPressed: () {
-                                  Navigator.of(ctx).pop();
-                                })
-                          ],
-                    ),
-                  ),
-                );
-              },
-              context: context,
-            );
-          },
-        ),
-        ElevatedButton(
-          child: const Text('Kill all running isolates'),
-          onPressed: () async {
-            await FlutterIsolate.killAll();
-          },
-        ),
-        ElevatedButton(
-          child: const Text('Run in compute function'),
-          onPressed: () async {
-            await flutterCompute(computeFunction, "foo");
-          },
-        ),
-      ]),
+      ),
     );
   }
 }
